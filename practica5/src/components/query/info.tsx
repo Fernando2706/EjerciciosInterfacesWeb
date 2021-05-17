a760e1ea85895a2c3a7f637cd5012203207880ef
Fernando2706 <fmurua28@gmail.com>
2021-05-17 09:47:44 +0200

    Entrega practica extra

--- practica5/src/components/query/info.tsx
+++ practica5/src/components/query/info.tsx
@@ -1,14 +1,20 @@
-import React, {useState, FC} from 'react';
+import React, { useState, FC, useEffect } from 'react';
 import ClipLoader from "react-spinners/ClipLoader";
 import { gql, useQuery } from "@apollo/client"
 import './info.css'
+import axios from 'axios';
 const City = gql`
   query getCity($name :String!){
       cities(where: {name: {eq: $name}}) {
         name
         population
+        location {
+          lat
+          long
+        }
         country {
           name
+          alpha2Code
         }
         timeZone {
           name
@@ -16,33 +22,107 @@ const City = gql`
       }
   }
 `
-interface CityProps{
+interface CityProps {
   name: string,
   changeFilter: Function,
 
 }
 
-interface ICity{
+interface ICity {
   cities: Array<{
     name: string,
     population: number,
+    location: {
+      lat: number,
+      long: number
+    }
     country: {
-      name:string
+      name: string,
+      alpha2Code: string
     }
     timeZone: {
-      name:string
+      name: string
     }
   }>
 }
 
-const InfoCity: FC<CityProps>= (props) => {
-  const {data,loading,error} = useQuery<ICity>(City,{variables:{name:props.name}})
+
+interface WeatherAPI {
+  coord: Coord;
+  weather: Weather[];
+  base: string;
+  main: Main;
+  visibility: number;
+  wind: Wind;
+  clouds: Clouds;
+  dt: number;
+  sys: Sys;
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
+interface Clouds {
+  all: number;
+}
+
+interface Coord {
+  lon: number;
+  lat: number;
+}
+
+interface Main {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+}
+
+interface Sys {
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+}
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface Wind {
+  speed: number;
+  deg: number;
+}
+
+
+
+
+const InfoCity: FC<CityProps> = (props) => {
+  const { data, loading, error } = useQuery<ICity>(City, { variables: { name: props.name } })
   const [index, setIndex] = useState<number>(-1);
+  const [dataWeather, setData] = useState<WeatherAPI>();
+  useEffect(() => {
+    if (index !== -1) {
+      axios.get("https://api.openweathermap.org/data/2.5/weather?lat=" + data?.cities[index].location.lat + "&lon=" + data?.cities[index].location.long + "&appid=" + process.env.REACT_APP_KEY).then((results) => {
+        setData(results.data);
+        console.log(results.data);
+
+      })
+    }
+
+  }, [index]);
   console.log(data);
-  
+
   return (
     <div className="container_global">
-      {loading&& <ClipLoader color="blue"/>}
+      {loading && <ClipLoader color="blue" />}
       {error && <div>{error.message}</div>}
       {data && data.cities.map((city, index_city) => {
         if (index === index_city) {
@@ -52,10 +132,10 @@ const InfoCity: FC<CityProps>= (props) => {
                 setIndex(index)
               }}>{city.name}</div>
               <div className="args country" onClick={() => {
-                props.changeFilter(city.country.name,true)
-              }}>Country: { city.country.name}</div>
-              <div className="args">Population: { city.population}</div>
-              {city.timeZone&&<div className="args">TimeZone: { city.timeZone.name}</div>}
+                props.changeFilter(city.country.name, true)
+              }}>Country: {city.country.name}</div>
+              <div className="args">Population: {city.population}</div>
+              {city.timeZone && <div className="args">TimeZone: {city.timeZone.name}</div>}
             </div>
           )
         } else {
@@ -64,10 +144,37 @@ const InfoCity: FC<CityProps>= (props) => {
               <div className="name" onClick={() => {
                 setIndex(index_city)
               }}>{city.name}</div>
-              </div>
+            </div>
           )
         }
       })}
+
+      {index !== -1 &&
+        <div className="ventana">
+          <div className="cerrar" onClick={() => setIndex(-1)}>X</div>
+          <div className="datos">
+            <div className="bandera">
+              <img src={"https://www.countryflags.io/" + data?.cities[index].country.alpha2Code + "/flat/64.png"}></img>
+
+            </div>
+          <div className="data country" onClick={
+            () => {
+              props.changeFilter(data?.cities[index].country.name,true);
+            }
+            }>Country: {data?.cities[index].country.name}</div>
+            <div className="data">Population: {data?.cities[index].population} </div>
+            {data?.cities[index].timeZone && <div className="data">Time zone: {data.cities[index].timeZone.name}</div>}
+            {dataWeather &&
+              <div>
+                <br />
+                <div className="data">Weather description: {dataWeather.weather[0].description}</div>
+                <div className="data">Time: {(dataWeather.main.temp - 273.15)}ºC</div>
+              </div>
+            }
+          </div>
+        </div>
+
+      }
     </div>
   )
 }
--- practica5/src/components/query/infoCountry.tsx
+++ practica5/src/components/query/infoCountry.tsx
@@ -1,7 +1,8 @@
-import React, {useState, FC} from 'react';
+import React, { useState, FC, useEffect } from 'react';
 import ClipLoader from "react-spinners/ClipLoader";
 import { gql, useQuery } from "@apollo/client"
 import './infoCountry.css'
+import axios from 'axios';
 
 
 const Country = gql`
@@ -19,59 +20,139 @@ const Country = gql`
       }
       capital {
         name
+        location {
+          lat
+          long
+        }
       }
       name
       alpha2Code
+      
     }
   }
     
   
 `
 
-interface CountryProps{
+interface CountryProps {
   name: string
   changeFilter: Function,
 }
 
-interface ICountry{
+interface ICountry {
   countries: Array<{
     continent: {
-      name:string
+      name: string
     },
     capital: {
-      name:string
+      name: string,
+      location: {
+        lat: number,
+        long: number
+      }
     },
     currencies: Array<{
-      name:string
+      name: string
     }>,
     population: number,
     languages: Array<{
-      name:string
+      name: string
     }>,
-    name:string,
-    alpha2Code:string
+    name: string,
+    alpha2Code: string,
+
   }>
 }
 
+interface WeatherAPI {
+  coord: Coord;
+  weather: Weather[];
+  base: string;
+  main: Main;
+  visibility: number;
+  wind: Wind;
+  clouds: Clouds;
+  dt: number;
+  sys: Sys;
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
+interface Clouds {
+  all: number;
+}
+
+interface Coord {
+  lon: number;
+  lat: number;
+}
+
+interface Main {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+}
+
+interface Sys {
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+}
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface Wind {
+  speed: number;
+  deg: number;
+}
+
+
+
 const InfoCountry: FC<CountryProps> = (props) => {
 
-  const {data,loading,error} = useQuery<ICountry>(Country,{variables:{name:props.name}})
+  const { data, loading, error } = useQuery<ICountry>(Country, { variables: { name: props.name } })
   const [index, setIndex] = useState<number>(-1);
+
+  const [dataWeather, setData] = useState<WeatherAPI>();
+
+  useEffect(() => {
+    if (index !== -1) {
+      axios.get("https://api.openweathermap.org/data/2.5/weather?lat=" + data?.countries[index].capital.location.lat + "&lon=" + data?.countries[index].capital.location.long + "&appid=" + process.env.REACT_APP_KEY).then((results) => {
+        setData(results.data);
+        console.log(results.data);
+
+      })
+    }
+
+  }, [index]);
 
   return (
     <div className="container_global_country">
-      {loading&& <ClipLoader color="blue"/>}
+      {loading && <ClipLoader color="blue" />}
       {error && <div>{error.message}</div>}
       {data && data.countries.map((country, index_country) => {
         if (index === index_country) {
           return (
             <div className="container_info_country" >
               <div className="name_country no" onClick={() => {
-                setIndex(index)
+                setIndex(index);
+
               }}>{country.name}</div>
               <div className="args_country">Continent: {country.continent.name}</div>
               <div className="capital" onClick={() => {
-                props.changeFilter(country.capital.name,false)
+                props.changeFilter(country.capital.name, false)
               }}>Capital: {country.capital.name}</div>
               <div className="args_country">Currencies: </div>
               {country.currencies && country.currencies.map((currencie) => {
@@ -84,7 +165,7 @@ const InfoCountry: FC<CountryProps> = (props) => {
               <div className="args_country">Population: {country.population}</div>
 
             </div>
-            
+
           )
         } else {
           return (
@@ -92,26 +173,41 @@ const InfoCountry: FC<CountryProps> = (props) => {
               <div className="name_country" onClick={() => {
                 setIndex(index_country)
               }}>{country.name}</div>
-              
 
-              </div>
+
+            </div>
           )
         }
       })}
-      {index!=-1&&
+      {index != -1 &&
         <div className="ventana">
-            <div className="cerrar" onClick={()=>setIndex(-1)}>X</div>
-            <div className="bandera">
-              <img src={"https://www.countryflags.io/"+data?.countries[index].alpha2Code+"/flat/64.png"}></img>
+          <div className="cerrar" onClick={() => setIndex(-1)}>X</div>
+          <div className="bandera">
+            <img src={"https://www.countryflags.io/" + data?.countries[index].alpha2Code + "/flat/64.png"}></img>
 
-            </div>
-            <div className="datos">
-              <div className="data">Name: {data?.countries[index].name}</div>
-              <div className="data">Code: {data?.countries[index].alpha2Code}</div>
-              <div className="data">Population: {data?.countries[index].population}</div>
-              <div className="data">Capital: {data?.countries[index].capital.name}</div>
-              <div className="data">Language: {data?.countries[index].languages[0].name}</div>
-            </div>
+          </div>
+          <div className="datos">
+            <div className="data">Continent: {data?.countries[index].continent.name}</div>
+            <div className="data">Name: {data?.countries[index].name}</div>
+            <div className="data">Code: {data?.countries[index].alpha2Code}</div>
+            <div className="data">Population: {data?.countries[index].population}</div>
+            <div className="data capital" onClick={() => {
+              props.changeFilter(data?.countries[index].capital.name, false)
+            }}>Capital: {data?.countries[index].capital.name}</div>
+            {data?.countries[index].languages && data.countries[index].languages.map((language, index) => {
+              return (<div className="data">Language {index}: {language.name}</div>)
+            })}
+            {data?.countries[index].currencies && data.countries[index].currencies.map((currency, index) => {
+              return (<div className="data">Currency {index}: {currency.name}</div>)
+            })}
+            {dataWeather &&
+              <div>
+                <br />
+                <div className="data">Weather description: {dataWeather.weather[0].description}</div>
+                <div className="data">Time: {(dataWeather.main.temp - 273.15)}ºC</div>
+              </div>
+            }
+          </div>
         </div>
       }
     </div>
